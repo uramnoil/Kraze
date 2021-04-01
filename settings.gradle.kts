811f032e9f73c7f2c651f7485549ffcb6778d521
uramnoil <uramnoil@outlook.jp>
2021-04-02 08:41:13 +0900

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+
+    repositories {
+        mavenCentral()
+    }
+
+    plugins {
+        kotlin("multiplatform") version kotlinVersion
+    }
+}
+
+rootProject.name = "Kraze"
