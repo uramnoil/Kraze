9a60d81bda3cf19641b07737d85dc191fa212572
uramnoil <uramnoil@outlook.jp>
2021-04-02 22:35:34 +0900

    #4 各オペレータのオーバーロードを追加

--- api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/math/IntVector3D.kt
+++ api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/math/IntVector3D.kt
@@ -11,4 +11,33 @@ import com.uramnoil.kraze.api.component.Component
  * @property z
  */
 data class IntVector3D(var x: Int, var y: Int, var z: Int) : Component {
+    operator fun plus(other: IntVector3D) = IntVector3D(
+        x + other.x,
+        y + other.y,
+        z + other.z
+    )
+
+    operator fun minus(other: IntVector3D) = IntVector3D(
+        x - other.x,
+        y - other.y,
+        z - other.z
+    )
+
+    operator fun times(other: IntVector3D) = IntVector3D(
+        x * other.x,
+        y * other.y,
+        z * other.z
+    )
+
+    operator fun div(other: IntVector3D) = IntVector3D(
+        x / other.x,
+        y / other.y,
+        z / other.z
+    )
+
+    operator fun rangeTo(other: IntVector3D) = IntVector3D(
+        other.x - x,
+        other.y - y,
+        other.z - y
+    )
 }
