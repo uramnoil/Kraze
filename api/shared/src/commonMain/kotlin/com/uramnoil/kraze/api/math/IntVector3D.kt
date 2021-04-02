bde9ef91b26c45670546c46241d23eac9e8f719b
uramnoil <uramnoil@outlook.jp>
2021-04-02 23:09:43 +0900

    #4 Float用のオーバーロードを作成

--- api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/math/IntVector3D.kt
+++ api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/math/IntVector3D.kt
@@ -41,12 +41,47 @@ data class IntVector3D(var x: Int, var y: Int, var z: Int) : Component {
         z / other.z
     )
 
+    operator fun plus(other: FloatVector3D) = FloatVector3D(
+        x + other.x,
+        y + other.y,
+        z + other.z
+    )
+
+    operator fun minus(other: FloatVector3D) = FloatVector3D(
+        x - other.x,
+        y - other.y,
+        z - other.z
+    )
+
+    operator fun times(other: FloatVector3D) = FloatVector3D(
+        x * other.x,
+        y * other.y,
+        z * other.z
+    )
+
+    operator fun div(other: FloatVector3D) = FloatVector3D(
+        x / other.x,
+        y / other.y,
+        z / other.z
+    )
+
     /**
-     * Vector from its own vector to the [other] vector.
+     * IntVector3D from its own vector to the [other] vector.
      *
      * @param other
      */
     operator fun rangeTo(other: IntVector3D) = IntVector3D(
+        other.x - x,
+        other.y - y,
+        other.z - z
+    )
+
+    /**
+     * FloatVector3D from its own vector to the [other] vector.
+     *
+     * @param other
+     */
+    operator fun rangeTo(other: FloatVector3D) = FloatVector3D(
         other.x - x,
         other.y - y,
         other.z - z
