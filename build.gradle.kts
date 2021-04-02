a5f87134c29f3b59d42e3d86d230cab70a1486ec
uramnoil <uramnoil@outlook.jp>
2021-04-02 10:12:06 +0900

    #1 kotlin-gradle-pluginプラグインを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,18 @@
 group = "com.uramnoil.kraze"
 version = "0.1.0"
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        val kotlin_version: String by project
+
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
 allprojects {
     repositories {
         mavenCentral()
