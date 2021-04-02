a11dccd3c8de8413fd2506f7a699d8d340152c8d
uramnoil <uramnoil@outlook.jp>
2021-04-02 10:46:25 +0900

    #1 ソースセットを設定

--- api/shared/build.gradle.kts
+++ api/shared/build.gradle.kts
@@ -1,7 +1,15 @@
+group = "com.uramnoil.kraze.api"
+
 plugins {
     kotlin("multiplatform")
 }
 
 kotlin {
     jvm {}
+
+    sourceSets {
+        val commonMain by getting {
+
+        }
+    }
 }
