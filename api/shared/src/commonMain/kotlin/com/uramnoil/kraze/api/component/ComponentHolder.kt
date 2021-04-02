2270535c2029eb02e927cb85912d381b682ef87c
uramnoil <uramnoil@outlook.jp>
2021-04-02 12:25:56 +0900

    #3 kdocを追加

--- api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/component/ComponentHolder.kt
+++ api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/component/ComponentHolder.kt
@@ -5,9 +5,36 @@ package com.uramnoil.kraze.api.component
  *
  */
 interface ComponentHolder {
+    /**
+     * A set of components.
+     *
+     * The concrete class has MutableList.
+     */
     val components: Set<Component>
+
+    /**
+     * Add a component.
+     *
+     * @param T Component type.
+     * @param component Components to add.
+     */
+    fun <T : Component> add(component: T)
+
+    operator fun Component.unaryPlus() {
+        add(this)
+    }
 }
 
-inline fun <reified T: Component> ComponentHolder.get() = components.filterIsInstance<T>().first()
+/**
+ * Returns a component that is instances of specified type parameter R
+ *
+ * @param T
+ */
+inline fun <reified T : Component> ComponentHolder.get() = components.filterIsInstance<T>().first()
 
-inline fun <reified T: Component> ComponentHolder.getOrNull() = components.filterIsInstance<T>().firstOrNull()
+/**
+ * Returns a component that is instances of specified type parameter R, or null if there are no components.
+ *
+ * @param T
+ */
+inline fun <reified T : Component> ComponentHolder.getOrNull() = components.filterIsInstance<T>().firstOrNull()
