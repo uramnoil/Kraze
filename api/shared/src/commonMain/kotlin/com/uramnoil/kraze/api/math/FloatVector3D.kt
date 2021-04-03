183cfb898e065bf31c13773433fba9074dd23d9c
uramnoil <uramnoil@outlook.jp>
2021-04-03 15:17:32 +0900

    #4 各オペレータを追加

--- api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/math/FloatVector3D.kt
+++ api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/math/FloatVector3D.kt
@@ -9,6 +9,82 @@ data class FloatVector3D(var x: Float, var y: Float, var z: Float) {
     val distanceSquared: Float
         get() = x * x + y * y + z + z
 
+    operator fun unaryMinus() = FloatVector3D(
+        -x,
+        -y,
+        -z
+    )
+
+    operator fun plus(other: IntVector3D) = FloatVector3D(
+        x + other.x,
+        y + other.y,
+        z + other.z
+    )
+
+    operator fun minus(other: IntVector3D) = FloatVector3D(
+        x - other.x,
+        y - other.y,
+        z - other.z
+    )
+
+    operator fun times(other: IntVector3D) = FloatVector3D(
+        x * other.x,
+        y * other.y,
+        z * other.z
+    )
+
+    operator fun div(other: IntVector3D) = FloatVector3D(
+        x / other.x,
+        y / other.y,
+        z / other.z
+    )
+
+    operator fun plus(other: FloatVector3D) = FloatVector3D(
+        x + other.x,
+        y + other.y,
+        z + other.z
+    )
+
+    operator fun minus(other: FloatVector3D) = FloatVector3D(
+        x - other.x,
+        y - other.y,
+        z - other.z
+    )
+
+    operator fun times(other: FloatVector3D) = FloatVector3D(
+        x * other.x,
+        y * other.y,
+        z * other.z
+    )
+
+    operator fun div(other: FloatVector3D) = FloatVector3D(
+        x / other.x,
+        y / other.y,
+        z / other.z
+    )
+
+    /**
+     * IntVector3D from its own vector to the [other] vector.
+     *
+     * @param other
+     */
+    operator fun rangeTo(other: IntVector3D) = FloatVector3D(
+        other.x - x,
+        other.y - y,
+        other.z - z
+    )
+
+    /**
+     * FloatVector3D from its own vector to the [other] vector.
+     *
+     * @param other
+     */
+    operator fun rangeTo(other: FloatVector3D) = FloatVector3D(
+        other.x - x,
+        other.y - y,
+        other.z - z
+    )
+
     fun vectorWhenXIsOnLine(other: FloatVector3D, x: Float): FloatVector3D? {
         val xDiff: Float = other.x - this.x
         val yDiff: Float = other.y - y
