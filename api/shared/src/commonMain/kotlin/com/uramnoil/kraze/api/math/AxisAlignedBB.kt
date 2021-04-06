0df586b76574dd2917a0fafec2ed3b683faffe3f
uramnoil <uramnoil@outlook.jp>
2021-04-06 20:14:33 +0900

    関数を移動

--- api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/math/AxisAlignedBB.kt
+++ api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/math/AxisAlignedBB.kt
@@ -387,3 +387,53 @@ class AxisAlignedBB
         return resultVector
     }
 }
+
+/**
+ * Get a new vector with the x axis set to the x parameter when the x parameter is
+ * on a line with this and the vector other
+ *
+ * @param other to check with
+ * @param x     which may on the line or not
+ * @return vector with x set or null when x is not on a line with this and the other vector
+ */
+fun FloatVector3D.vectorWhenXIsOnLine(other: FloatVector3D, x: Float): FloatVector3D? {
+    val xDiff: Float = other.x - this.x
+    val yDiff: Float = other.y - y
+    val zDiff: Float = other.z - z
+    return ((x - this.x) / xDiff)
+        .takeIf { it in 0f..1f }
+        ?.let { FloatVector3D(this.x + xDiff * it, y + yDiff * it, z + zDiff * it) }
+}
+
+/**
+ * Get a new vector with the y axis set to the y parameter when the y parameter is
+ * on a line with this and the vector other
+ *
+ * @param other to check with
+ * @param y     which may on the line or not
+ * @return vector with y set or null when y is not on a line with this and the other vector
+ */
+fun FloatVector3D.vectorWhenYIsOnLine(other: FloatVector3D, y: Float): FloatVector3D? {
+    val xDiff: Float = other.x - x
+    val yDiff: Float = other.y - this.y
+    val zDiff: Float = other.z - z
+    return ((y - this.y) / yDiff)
+        .takeIf { it in 0f..1f }?.let { FloatVector3D(x + xDiff * it, this.y + yDiff * it, z + zDiff * it) }
+}
+
+/**
+ * Get a new vector with the z axis set to the z parameter when the z parameter is
+ * on a line with this and the vector other
+ *
+ * @param other to check with
+ * @param z     which may on the line or not
+ * @return vector with y set or null when y is not on a line with this and the other vector
+ */
+fun FloatVector3D.vectorWhenZIsOnLine(other: FloatVector3D, z: Float): FloatVector3D? {
+    val xDiff: Float = other.x - x
+    val yDiff: Float = other.y - y
+    val zDiff: Float = other.z - this.z
+    return (z - this.z / zDiff)
+        .takeIf { it in 0f..1f }
+        ?.let { FloatVector3D(x + xDiff * it, y + yDiff * it, this.z + zDiff * it) }
+}
