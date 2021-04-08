b9328845020184d486237d15cd5aa1131d172bed
uramnoil <uramnoil@outlook.jp>
2021-04-09 01:01:48 +0900

    整理 (#6)

--- api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/entity/Entity.kt
+++ api/shared/src/commonMain/kotlin/com/uramnoil/kraze/api/entity/Entity.kt
@@ -1,27 +1,24 @@
 package com.uramnoil.kraze.api.entity
 
+import com.uramnoil.kraze.api.component.ComponentHolder
+import com.uramnoil.kraze.api.math.FloatVector3D
 import com.uramnoil.kraze.api.world.Location
 
-interface Entity {
+interface Entity : ComponentHolder {
     /**
      * Location with the entity.
      */
-    var location: Location
+    val location: Location
 
     /**
-     * Location where the Entity will spawn.
+     * The direction in which the body is facing.
      */
-    var spawnLocation: Location
+    var direction: FloatVector3D
 
     /**
-     * Tags that appear above the entity
+     * 1tick move.
      */
-    var nameTag: String
-
-    /**
-     * Whether to display the [nameTag] above the entity.
-     */
-    var isNameTagVisible: Boolean
+    val movement: FloatVector3D
 
     /**
      * Whether the entity is visible to others.
@@ -29,12 +26,51 @@ interface Entity {
     var isVisible: Boolean
 
     /**
-     * Whether the entity is immovable or not.
-     */
-    var isImmobile: Boolean
-
-    /**
      * Whether collision is possible in collision box.
      */
     var canCollide: Boolean
+
+    /**
+     * Whether it is grounded or not.
+     */
+    val onGround: Boolean
+
+    /**
+     * Entity size.
+     */
+    var scale: Float
+
+    /**
+     * Teleport to [to] in the same world.
+     *
+     * @param to
+     */
+    fun teleport(to: FloatVector3D)
+
+    /**
+     * Teleport to [to].
+     *
+     * @param to
+     */
+    fun teleport(to: Location)
+}
+
+/**
+ * Move to [to] relative to the speed of [speed].
+ *
+ * @param to
+ * @param speed meter per tick
+ */
+fun Entity.moveRelativelyTo(to: FloatVector3D, speed: Float) {
+    TODO()
+}
+
+/**
+ * Move to [to] absolute to the speed of [speed].
+ *
+ * @param to
+ * @param speed meter per tick
+ */
+fun Entity.moveAbsolutelyTo(to: FloatVector3D, speed: Float) {
+    TODO()
 }
